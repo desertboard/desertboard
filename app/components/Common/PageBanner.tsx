5142096d079c720b594c1f90421b03631eacc500
rahees <raheez1995india@gmail.com>
2025-01-27 14:13:56 +0530

    test

--- app/components/Common/PageBanner.tsx
+++ app/components/Common/PageBanner.tsx
@@ -1,34 +1,38 @@
 import React from "react";
 import Image from "next/image";
 import { StaticImageData } from "next/image";
+import "@/app/components/Common/pgbnr.scss";
 
 interface HeroSectionProps {
   bannerSrc: StaticImageData | string;
   arrowSrc: string;
   title: string;
+  desc?: string;
   breadcrumbs: { label: string; href: string }[];
+  bnrHeight?: string;
 }
 
-const HeroSection: React.FC<HeroSectionProps> = ({ bannerSrc, arrowSrc, title, breadcrumbs }) => {
+const HeroSection: React.FC<HeroSectionProps> = ({ bannerSrc, arrowSrc, title, desc, breadcrumbs}) => {
   return (
-    <section className="relative h-[60vh] bg-cover bg-center flex items-center justify-center text-center bnr-pg w-full">
+    // <section className="relative h-[75dvh] bg-cover bg-center flex items-center justify-center text-center bnr-pg pg-bnr w-full">
+    <section className={`relative bg-cover bg-center flex items-center justify-center text-center bnr-pg pg-bnr w-full`}>
       {/* <div className="absolute inset-0 bg-black opacity-60 -z-10"></div>
-      <div className="absolute inset-0 bg-primary opacity-20 -z-20"></div> */}
-      <figure className="absolute h-[60vh] w-full -z-40">
-        <Image className="w-full h-[60vh] absolute object-cover" src={bannerSrc} width={1000} height={800} alt="Banner image" />
+      <div className="absolute inset-0 bg-primary opacity-20 -z-20"></div>  */}
+      <figure className="absolute left-0 top-0 h-full w-full -z-40">
+        <Image className="w-full h-full absolute object-cover" src={bannerSrc} width={1000} height={800} alt="Banner image" />
       </figure>
       <div className="container relative">
         <div className="pg-bnr__div absolute bottom-0 pb-[80px]">
-          <h1 className="pg-bnr__ttl text-[72px] mb-[40px]">
+          <h1 className="pg-bnr__ttl text-font72 mb-10 font-bold leading-[1]">
             {title}
             <span className="text-[#FF671F]">.</span>
           </h1>
-
+          <p className="text-[#FFFFFFBF] text-font20 lh-1p33 mb-10 max-w-[40ch] text-left">{desc}</p>
           <div className="pg-bnr__txt flex items-center flex-wrap">
             <ul className="flex items-center">
               {breadcrumbs.map((breadcrumb, index) => (
                 <li key={index} className="inline-flex items-center">
-                  <a href={breadcrumb.href} className={`text-[#FFFFFFBF]  text-[20px] ${index === breadcrumbs.length - 1 ? "font-bold text-white " : "opacity-75"}`}>
+                  <a href={breadcrumb.href} className={`text-[#FFFFFFBF]  text-font20 ${index === breadcrumbs.length - 1 ? "font-bold text-white " : "opacity-75"}`}>
                     {breadcrumb.label}
                   </a>
                   {index < breadcrumbs.length - 1 && <Image src={arrowSrc} alt="Arrow" className="mx-2" />}
--- app/components/home/HeroSection.tsx
+++ app/components/home/HeroSection.tsx
@@ -19,25 +19,12 @@ const HeroSection = () => {
         <div className="absolute inset-0 bg-black opacity-60 -z-10"></div>
         <div className="absolute inset-0 bg-primary opacity-20 -z-20"></div>
         <figure className="absolute h-screen w-screen  -z-40">
-          <Image
-            className="w-screen h-screen absolute object-cover"
-            src={Banner}
-            width={1000}
-            height={800}
-            alt=""
-          />
+          <Image className="w-screen h-screen absolute object-cover" src={Banner} width={1000} height={800} alt="" />
         </figure>
         <div className="relative z-10 text-white px-6 max-w-4xl">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-[17ch]">
-            DesertBoard’s Palm Strand Board
-          </h1>
-          <p className="text-lg md:text-xl font-light mb-6 opacity-[75%]">
-            The region’s most sustainable building material
-          </p>
-          <Link
-            href="#"
-            className="rounded-none inline-block px-6 py-3 border border-white text-white hover:bg-white hover:text-black transition rounded-md"
-          >
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 max-w-[17ch]">DesertBoard’s Palm Strand Board</h1>
+          <p className="text-lg md:text-xl font-light mb-6 opacity-[75%]">The region’s most sustainable building material</p>
+          <Link href="#" className="rounded-none inline-block px-6 py-3 border border-white text-white hover:bg-white hover:text-black transition rounded-md">
             Know More
           </Link>
         </div>
@@ -50,55 +37,23 @@ const HeroSection = () => {
               <h2 className="  text-gray-900 mb-4 text-[48px]  font-black">
                 Inspiration<span className="text-[#FF671F]">.</span>
               </h2>
-              <p className="text-lg text-gray-700 mb-6 text-[#151515] opacity-[50%]">
-                A Legacy Rooted in the Desert
-              </p>
-              <p className="text-[20px]">
-                In the heart of the desert, where towering date palm trees
-                symbolize our heritage, a groundbreaking innovation has emerged:
-                DesertBoard's Palm Strand Board (PSB®), the world’s first
-                engineered palm-based board.{" "}
-              </p>
+              <p className="text-lg text-gray-700 mb-6 text-[#151515] opacity-[50%]">A Legacy Rooted in the Desert</p>
+              <p className="text-[20px]">In the heart of the desert, where towering date palm trees symbolize our heritage, a groundbreaking innovation has emerged: DesertBoard&apos;s Palm Strand Board (PSB®), the world’s first engineered palm-based board. </p>
               <br />
               <p className="text-[20px]">
-                For centuries, date palm trees have been a vital resource in the
-                Middle East, historically used to construct Barasti houses that
-                provided essential shelter in the harsh desert climate. Inspired
-                by the rich legacy and the vision of the UAE's founding father,
-                Sheikh Zayed bin Sultan Al Nahyan, DesertBoard® successfully
-                produced the first engineered board in 2021.
+                For centuries, date palm trees have been a vital resource in the Middle East, historically used to construct Barasti houses that provided essential shelter in the harsh desert climate. Inspired by the rich legacy and the vision of the UAE&apos;s founding father, Sheikh Zayed bin Sultan Al Nahyan, DesertBoard® successfully produced
+                the first engineered board in 2021.
               </p>
             </div>
 
             <div className="  flex justify-end relative top-10">
               <div className="relative">
-                <Image
-                  src={imsec2}
-                  width={1080}
-                  height={640}
-                  alt="Picture of the author"
-                />
+                <Image src={imsec2} width={1080} height={640} alt="Picture of the author" />
                 <div className="absolute inset-0 flex items-center justify-center">
                   <button className="bg-black bg-opacity-50 rounded-full p-4">
-                    <svg
-                      className="w-8 h-8 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M14.752 11.168l-3.197-2.132A1 1 0 0010 10v4a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                      />
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M21 12c0 4.97-4.03 9-9 9s-9-4.03-9-9 4.03-9 9-9 9 4.03 9 9z"
-                      />
+                    <svg className="w-8 h-8 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 10v4a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12c0 4.97-4.03 9-9 9s-9-4.03-9-9 4.03-9 9-9 9 4.03 9 9z" />
                     </svg>
                   </button>
                 </div>
@@ -113,20 +68,12 @@ const HeroSection = () => {
             <h2 className="text-3xl font-bold text-gray-800 mb-2">
               Sectors<span className="text-[#FF671F]">.</span>
             </h2>
-            <p className="text-gray-400 text-[20px] max-w-[75ch]">
-              PSB® boards' diverse product lineup is used in everything from
-              house building, furniture, and interior design to mass timber
-              structures, fire-rated doors, building facades, and flooring.
-            </p>
+            <p className="text-gray-400 text-[20px] max-w-[75ch]">PSB® boards&apos; diverse product lineup is used in everything from house building, furniture, and interior design to mass timber structures, fire-rated doors, building facades, and flooring.</p>
           </div>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             <div className="relative group bg-gray-800 overflow-hidden">
-              <Image
-                src={gdim1}
-                alt="Engineering & Construction"
-                className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300"
-              />
+              <Image src={gdim1} alt="Engineering & Construction" className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300" />
               <div className="absolute inset-0 bg-blacktrans bg-opacity-50 group-hover:bg-opacity-30 transition-all duration-300"></div>
               <div className="absolute top-0 text-white w-full p-8">
                 <div className="w-full ">
@@ -135,84 +82,57 @@ const HeroSection = () => {
                 <h3 className=" pt-5 border-t opacity-[90%] text-lg font-semibold mt-5 text-[28px] leading-[1.3] font-[700] transition-all duration-300 group-hover:mt-10">
                   Engineering <br /> & Construction
                 </h3>
-                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">
-                  Discover how our innovative solutions streamline engineering
-                  and construction projects. From planning to execution, we
-                  ensure precision and efficiency.
-                </p>
-                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">Read More</a>
+                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">Discover how our innovative solutions streamline engineering and construction projects. From planning to execution, we ensure precision and efficiency.</p>
+                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">
+                  Read More
+                </a>
               </div>
             </div>
 
             <div className="relative group bg-gray-800 overflow-hidden">
-              <Image
-                src={gdim2}
-                alt="Landscape"
-                className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300"
-              />
+              <Image src={gdim2} alt="Landscape" className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300" />
               <div className="absolute inset-0 bg-blacktrans bg-opacity-50 group-hover:bg-opacity-30 transition-all duration-300"></div>
               <div className="absolute top-0 text-white w-full p-8">
                 <div className="w-full ">
                   <Image src={icn2} alt="icn1" className="" />
                 </div>
-                <h3 className=" pt-5 border-t opacity-[90%] text-lg font-semibold mt-5 text-[28px] leading-[1.3] font-[700] transition-all duration-300 group-hover:mt-10">
-                Landscape
-                </h3>
-                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">
-                  Discover how our innovative solutions streamline engineering
-                  and construction projects. From planning to execution, we
-                  ensure precision and efficiency.
-                </p>
-                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">Read More</a>
-
+                <h3 className=" pt-5 border-t opacity-[90%] text-lg font-semibold mt-5 text-[28px] leading-[1.3] font-[700] transition-all duration-300 group-hover:mt-10">Landscape</h3>
+                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">Discover how our innovative solutions streamline engineering and construction projects. From planning to execution, we ensure precision and efficiency.</p>
+                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">
+                  Read More
+                </a>
               </div>
             </div>
 
             <div className="relative group bg-gray-800 overflow-hidden">
-              <Image
-                src={gdim3}
-                alt="Interior Design"
-                className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300"
-              />
+              <Image src={gdim3} alt="Interior Design" className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300" />
               <div className="absolute inset-0 bg-blacktrans bg-opacity-50 group-hover:bg-opacity-30 transition-all duration-300"></div>
               <div className="absolute top-0 text-white w-full p-8">
                 <div className="w-full ">
                   <Image src={icn3} alt="icn1" className="" />
                 </div>
-                <h3 className=" pt-5 border-t opacity-[90%] text-lg font-semibold mt-5 text-[28px] leading-[1.3] font-[700] transition-all duration-300 group-hover:mt-10">
-                Interior Design
-                </h3>
-                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">
-                  Discover how our innovative solutions streamline engineering
-                  and construction projects. From planning to execution, we
-                  ensure precision and efficiency.
-                </p>
-                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">Read More</a>
-
+                <h3 className=" pt-5 border-t opacity-[90%] text-lg font-semibold mt-5 text-[28px] leading-[1.3] font-[700] transition-all duration-300 group-hover:mt-10">Interior Design</h3>
+                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">Discover how our innovative solutions streamline engineering and construction projects. From planning to execution, we ensure precision and efficiency.</p>
+                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">
+                  Read More
+                </a>
               </div>
             </div>
 
             <div className="relative group bg-gray-800 overflow-hidden">
-              <Image
-                src={gdim4}
-                alt="Events & Exhibitions"
-                className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300"
-              />
+              <Image src={gdim4} alt="Events & Exhibitions" className="w-full h-[552px] object-cover opacity-80 group-hover:opacity-100 transition-all duration-300" />
               <div className="absolute inset-0 bg-blacktrans bg-opacity-50 group-hover:bg-opacity-30 transition-all duration-300"></div>
               <div className="absolute top-0 text-white w-full p-8">
                 <div className="w-full ">
                   <Image src={icn4} alt="icn1" className="" />
                 </div>
                 <h3 className=" pt-5 border-t opacity-[90%] text-lg font-semibold mt-5 text-[28px] leading-[1.3] font-[700] transition-all duration-300 group-hover:mt-10">
-                Events   <br /> & Exhibitions
+                  Events <br /> & Exhibitions
                 </h3>
-                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">
-                  Discover how our innovative solutions streamline engineering
-                  and construction projects. From planning to execution, we
-                  ensure precision and efficiency.
-                </p>
-                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">Read More</a>
-
+                <p className="mt-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5]">Discover how our innovative solutions streamline engineering and construction projects. From planning to execution, we ensure precision and efficiency.</p>
+                <a href="#" className="relative top-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-[16px] leading-[1.5] rmbtn ">
+                  Read More
+                </a>
               </div>
             </div>
           </div>
