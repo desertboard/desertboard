dae4e9440d9943619654cc1c08206375be7efd46
jishnum007 <jishnu@globalsurf.ae>
2025-02-13 12:53:47 +0530

    home page issues

--- app/components/NewsEvents/NewsBlock.tsx
+++ app/components/NewsEvents/NewsBlock.tsx
@@ -23,7 +23,9 @@ interface NewsEventsProps {
 
 }
 
-
+const formatText = (text: string) => {
+  return text.replace(/®/g, "<sup>®</sup>");
+};
 
 const NewsBlock = ({ sectionTitle,data}:NewsEventsProps) => {
 
@@ -50,8 +52,9 @@ const NewsBlock = ({ sectionTitle,data}:NewsEventsProps) => {
                     <div className="news-crd__content bg-[#fbf5f0] font-helvetica p-6">
                       <h4 className="text-black text-font14 opacity-75 leading-normal mb-3 uppercase font-bold">{news.date}</h4>
                       {/* <h3 className="text-font20 xl:text-font28 text-Darkgreen font-bold leading-[1.3] mb-2 lg:mb-5 overflow-hidden text-ellipsis display-webkit-box line-clamp-2 webkit-box-orient-vertical" dangerouslySetInnerHTML={{ __html: news.title }}></h3> */}
-                      <h3 className="text-font20 xxl:text-font24 text-Darkgreen font-bold leading-[1.3] mb-2 lg:mb-5 overflow-hidden text-ellipsis helvetica display-webkit-box line-clamp-2 webkit-box-orient-vertical">{news.title}</h3>
-                      <div className="text-font19 xxl:text-font20 leading-normal text-black opacity-75 mb-5 overflow-hidden text-ellipsis display-webkit-box line-clamp-3 webkit-box-orient-vertical">{parse(news.description)}</div>
+                      <h3 className="text-font20 xxl:text-font24 text-Darkgreen font-bold leading-[1.3] mb-2 lg:mb-5 overflow-hidden text-ellipsis helvetica display-webkit-box line-clamp-2 webkit-box-orient-vertical" dangerouslySetInnerHTML={{ __html: formatText(news.title)}}></h3>
+                      <div className="text-font19 xxl:text-font20 leading-normal text-black opacity-75 mb-5 overflow-hidden text-ellipsis display-webkit-box line-clamp-3 webkit-box-orient-vertical" dangerouslySetInnerHTML={{ __html: formatText(news.description) }}></div>
+
                       <div className="flex flex-wrap gap-2 mb-6">
                         <Image src={tagIcon} width={20} height={20} alt="categories" className="w-[15px] h-[15px] xl:w-[18px] xl:h-[18px]" />
                         <ul className="news__category list-none text-black uppercase font-bold text-font14 leading-normal flex gap-3 opacity-75">
