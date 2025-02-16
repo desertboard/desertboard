f8d132f94b905955ec2292c29e58873b9fab52c5
jishnum007 <jishnu@globalsurf.ae>
2025-02-16 20:10:00 +0530

    build fix

--- app/components/Sustainability/swipersustain.tsx
+++ app/components/Sustainability/swipersustain.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import { Navigation, Pagination } from "swiper/modules";
