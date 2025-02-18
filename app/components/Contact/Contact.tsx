c4314d4babdada04d09051fc8eb6ca57d1a80c8d
Nived <nivuyt789@gmail.com>
2025-02-18 13:33:52 +0530

    fix:build

--- app/components/Contact/Contact.tsx
+++ app/components/Contact/Contact.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect } from 'react'
+import React from 'react'
 import bannerImg from "@/public/assets/contactbanner.jpg";
 import PageBanner from '../Common/PageBanner';
 import Arrow from "@/public/assets/brdcrbs.svg";
