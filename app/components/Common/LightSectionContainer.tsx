dbaf6dd7eed704bda32528786d3df33216dc37aa
yasirtlr <desertboarddxb@gmail.com>
2025-02-18 12:12:57 +0400

    avc

--- app/components/Common/LightSectionContainer.tsx
+++ app/components/Common/LightSectionContainer.tsx
@@ -14,7 +14,7 @@ const LightSectionContainer = ({children}:{
         <motion.div className="ola ola-right absolute top-0 right-[-10%] w-[20em] md:w-[40em] " animate={{ y: [0, -20, 0], rotate: [0, -1, 0] }} transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}>
           <Image className="absolute w-full " src={lfbef} alt="Description of the image" width={300} height={300} />
         </motion.div>
-        <motion.div className="ola ola-right absolute bottom-[43%] left-[-15%] w-[20em] md:w-[40em]" animate={{ y: [0, -20, 0], rotate: [0, 2, 0] }} transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}>
+        <motion.div className="ola ola-right absolute bottom-[43%] left-[-15%] w-[20em] md:w-[40em] -z-10" animate={{ y: [0, -20, 0], rotate: [0, 2, 0] }} transition={{ duration: 6, repeat: Infinity, ease: "easeInOut" }}>
           <Image className="absolute w-full" src={lfbt} alt="Description of the image" width={300} height={300} />
         </motion.div>
         {children}
