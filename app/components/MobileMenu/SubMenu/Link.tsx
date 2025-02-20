0926dce116a367131e7f35e9703cdf1ac0aeaeba
jishnum007 <jishnu@globalsurf.ae>
2025-02-20 17:08:04 +0530

    menu and sustainablity to

--- app/components/MobileMenu/SubMenu/Link.tsx
+++ app/components/MobileMenu/SubMenu/Link.tsx
@@ -15,25 +15,25 @@ export default function Index({data, isActive, setSelectedIndicator,setIsActive,
     setIsActive:Dispatch<SetStateAction<boolean>>
     setSubMenuActive:Dispatch<SetStateAction<boolean>>
 }) {
-  
+
     const { title, href, index} = data;
-  
+
     return (
-      <motion.div 
-        className={styles.link} 
-        onMouseEnter={() => {setSelectedIndicator(href)}} 
-        custom={index} 
-        variants={slide} 
-        initial="initial" 
-        animate="enter" 
+      <motion.div
+        className={styles.link}
+        onMouseEnter={() => {setSelectedIndicator(href)}}
+        custom={index}
+        variants={slide}
+        initial="initial"
+        animate="enter"
         exit="exit"
       >
-        <motion.div 
-          variants={scale} 
-          animate={isActive ? "open" : "closed"} 
+        <motion.div
+          variants={scale}
+          animate={isActive ? "open" : "closed"}
           className={styles.indicator}>
         </motion.div>
-        <Link href={href} className='text-2xl mb-3' onClick={()=>{setIsActive(false);setSubMenuActive(false)}}>{title}</Link>
+        <Link href={href} className='text-2xl mb-3 linkmns' onClick={()=>{setIsActive(false);setSubMenuActive(false)}}>{title}</Link>
       </motion.div>
     )
 }
