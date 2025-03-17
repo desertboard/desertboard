6cd0a5de7f7ea31a6ac0613543db56938d2649ad
Nived <nivuyt789@gmail.com>
2025-03-17 16:00:22 +0530

    usp section small screen fix

--- app/components/home/UspItem.tsx
+++ app/components/home/UspItem.tsx
@@ -12,14 +12,14 @@ const formatText = (text: string) => {
 };
 const UspItem = ({ uspTitle, order, mainImg, uspDesc, uspIcon, onMouseEnter }: UspProps) => {
     return (
-      <div className={`usp-item usp-${order} relative overflow-hidden hover:pb-7 group`} onMouseEnter={onMouseEnter}>
+      <div className={`usp-item usp-${order} relative overflow-hidden lg:hover:pb-7 pb-0 group`} onMouseEnter={onMouseEnter}>
         <div className="usp-item__bg absolute inset-0 overflow-hidden">
           <Image src={mainImg} alt="usp" className="w-full h-full object-cover" fill></Image>
         </div>
         <div className="usp-item__i absolute top-5 left-5 xl:top-10 xl:left-10 z-20">
           <Image src={uspIcon} alt="usp" className="" width={40} height={40}></Image>
         </div>
-        <h3 className="xxl:text-font28 leading-[1.3] nuber-next-bold absolute bottom-[25px] z-[1]  px-4 xxl:px-8 intitle">{uspTitle}</h3>
+        {/* <h3 className="xxl:text-font28 leading-[1.3] nuber-next-bold absolute bottom-[25px] z-[1]  px-4 xxl:px-8 intitle">{uspTitle}</h3> */}
         <div className="usp-item__content relative z-10 px-4 xxl:px-8">
           <div className='usp-item__desc pt-4 xxl:pt-7'>
           <h3 className="xxl:text-font28 leading-[1.3] nuber-next-bold    delay-200 group-hover:translate-y-[10px] transition-all duration-500 ease-linear">{uspTitle}</h3>
