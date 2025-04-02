e341ad8e56548352fff1eb137d62a54b85585ebe
Nived <nivuyt789@gmail.com>
2025-04-02 12:26:08 +0530

    GTM script added

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -55,6 +55,20 @@ export default function RootLayout({
         /> */}
         {/* Google Tag Manager Script */}
 
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NZPT95ZG');
+            `,
+          }}
+        />
+
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -66,6 +80,16 @@ export default function RootLayout({
       </Script>
       </head>
       <body className={`antialiased overflow-x-hidden`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NZPT95ZG"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager */}
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=AW-16905314007"
